f9b24b22785f505d61ab91e132ac1200a3374eb8
Gorddd <kasual2014@icloud.com>
2021-12-30 21:45:20 +0300

    add class Pixel

---
+++ GordSnake/Pixel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace GordSnake
+{
+    public static class PixelChars
+    {
+        public const char Brick = '▓';
+    }
+
+
+    public readonly struct Pixel
+    {
+        public Pixel(int x, int y, ConsoleColor color, char pixelChar)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+            PixelChar = pixelChar;
+        }
+
+        public int X{ get; }
+
+        public int Y { get; }
+
+        public ConsoleColor Color { get; }
+
+        public char PixelChar { get; }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(PixelChar);
+        }
+
+        public void Clear()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(' ');
+        }
+    }
+}
