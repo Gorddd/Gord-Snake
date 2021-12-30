bf468839d2c4b82ffbd48d765fa6be05cb2f06a3
Gorddd <kasual2014@icloud.com>
2021-12-30 21:05:41 +0300

    Добавьте файлы проекта.

---
+++ GordSnake/Area.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace GordSnake
+{
+    public class Area
+    {
+        private int mapWidth;
+        private int mapHeight;
+
+        public Area(int mapWidth, int mapHeight)
+        {
+            Console.CursorVisible = false;
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+
+            Console.SetWindowSize(mapWidth, mapHeight);
+            Console.SetBufferSize(mapWidth, mapHeight);
+        }
+    }
+}
---
+++ GordSnake/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GordSnake
+{
+    class Program
+    {
+        static void Main()
+        {
+            var area = new Area(30, 20);
+
+            Console.ReadKey();
+        }
+    }
+}
