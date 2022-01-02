8a3176e7c61933a69db10f073430ba55e04d84cb
Gorddd <kasual2014@icloud.com>
2022-01-02 14:42:30 +0300

    add border

--- GordSnake/Area.cs
+++ GordSnake/Area.cs
@@ -10,12 +10,26 @@ namespace GordSnake
 
         public Area(int mapWidth, int mapHeight)
         {
-            Console.CursorVisible = false;
             this.mapWidth = mapWidth;
             this.mapHeight = mapHeight;
 
             Console.SetWindowSize(mapWidth, mapHeight);
             Console.SetBufferSize(mapWidth, mapHeight);
+        }
+
+        public void DrawBorder()
+        {
+            for (int i = 0; i < mapWidth; i++)
+            {
+                new Pixel(i, 1, ConsoleColor.Gray, PixelChars.Brick).Draw();
+                new Pixel(i, mapHeight - 1, ConsoleColor.Gray, PixelChars.Brick).Draw();
+            }
+
+            for (int i = 1; i < mapHeight - 1; i++)
+            {
+                new Pixel(0, i, ConsoleColor.Gray, PixelChars.Brick).Draw();
+                new Pixel(mapWidth - 1, i, ConsoleColor.Gray, PixelChars.Brick).Draw();
+            }
         }
     }
 }
--- GordSnake/Program.cs
+++ GordSnake/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Text;
 
 namespace GordSnake
 {
@@ -6,7 +8,14 @@ namespace GordSnake
     {
         static void Main()
         {
+            Console.CursorVisible = false;
             var area = new Area(30, 20);
+            area.DrawBorder();
+
+            //Font.SetConsoleFont();
+            //int value = 977788899;
+            //Console.OutputEncoding = Encoding.UTF8;
+            //Console.WriteLine("Price: " + value.ToString("C", CultureInfo.CreateSpecificCulture("fr-FR")));
 
             Console.ReadKey();
         }
